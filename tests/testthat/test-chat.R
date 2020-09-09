0dddf726017850dc5872d6ce906e72d786279f55
Jonathan Sidi <yonis@metrumrg.com>
2020-09-08 20:20:05 -0400

    refactor

---
+++ tests/testthat/test-chat.R
@@ -0,0 +1,58 @@
+# Environment Variable Must be Defined (Either by Local .Renviron or Environment variable on CI.)
+token <- Sys.getenv('SLACK_API_TOKEN')
+
+channel <- "DNRKMTFGD"
+
+testthat::describe("chat upload", {
+
+  test_result <- chat_slack(
+       channel = channel,
+       token = token,
+       text = 'test'
+  )
+
+  it('ok result',{
+    testthat::expect_true(test_result$ok)
+  })
+
+ it('names of return',{
+   expect_identical(
+     names(test_result),
+     c('ok','channel','ts','message')
+    )
+ })
+
+ it('last_post returns attribs',{
+   expect_identical(
+     structure(list(
+     ts  = test_result$ts,
+     channel = test_result$channel
+     ),class = 'slackpost'),
+     post_last())
+ })
+
+ it('query stack',{
+   expect_true(inherits(post_stack()[[1]],'slackpost'))
+ })
+
+ it('remove post',{
+
+   lp <- post_last()
+
+   test_remove <- chat_slack(
+     slack_method = 'chat.delete',
+     token = token,
+     channel = lp$channel,
+     ts = lp$ts,
+     action = 'pop')
+
+    testthat::expect_true(test_remove$ok)
+
+ })
+
+ it('empty stack',{
+
+   testthat::expect_warning(post_last(),'No posts in stack')
+
+ })
+})
